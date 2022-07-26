269d4961afb9bad968029d0bfe786ad359e56166
Esokami <mollica.es@gmail.com>
2022-07-25 20:41:01 -0700

    API/Character Creation
    Can use populate list of API names to make a character

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import CharacterList from './components/CharacterList';
 import ViewCharacter from './components/ViewCharacter';
 import Register from './components/Register';
 import Login from './components/Login';
+import AddCharacter from './components/AddCharacter';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
+          <Route path="/characters/add" element={<AddCharacter/>}/>
           <Route path="/characters" element={<CharacterList/>}/>
           <Route path="/characters/:id" element={<ViewCharacter/>}/>
           <Route path="/register" element={<Register/>}/>
