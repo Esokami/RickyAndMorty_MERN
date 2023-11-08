c067f6707c2d969d3109ade4104975fc96b0c160
Esokami <mollica.es@gmail.com>
2023-11-08 08:11:17 -0800

    Initial Heroku deployment

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose
-    .connect(`mongodb://localhost/${process.env.DB_NAME}`, {
+    .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     })
--- server/server.js
+++ server/server.js
@@ -1,17 +1,37 @@
 require("dotenv").config();
+const mongoose = require('mongoose');
 const express = require("express");
 const cors = require("cors");
 const app = express();
 const cookieParser = require('cookie-parser');
+const PORT = process.env.PORT || '8080';
 
-app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+app.set("port", PORT);
+
+app.use(cors({origin: PORT, credentials: true}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.listen(8000, () => {
-    console.log("Listening at Port 8000");
-});
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '..', 'client/build')));
+    app.get(/^\/(?!api).*/, (req, res) => {
+        res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'))
+    });
+}
+
+app.listen(
+    PORT,
+    console.log(`Server running on ${process.env.NODE_ENV}`)
+);
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+    .then(() => console.log("Established a connection to the database"))
+    .catch(err => console.log("Something went wrong when connecting to the database", err));
 
 require("./config/mongoose.config");
 require("./routes/character.routes")(app);
